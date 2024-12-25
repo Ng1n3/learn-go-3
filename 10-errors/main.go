072e4adf09a47a52cdf20ac0cec27c8a119a8767
ng1n3 <code.with.muyiwa@gmail.com>
2024-12-25 23:56:01 +0100

    update: error exercises

--- 10-errors/main.go
+++ 10-errors/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"archive/zip"
-	"bytes"
 	"errors"
+	panicrecover "errors/panicRecover"
 	"fmt"
+
+	"github.com/pkg/errors"
 )
 
 type error interface {
@@ -16,14 +17,40 @@ func Lecture() {
 	//[lecture] -> The reason you return nill from a function is to indicate that no error occurred is that nil is the zero value for pointers, interfaces, maps, slices, channels and function types, representing an uninitialized value.
 	//[Lecture] -> You can call errors using error.New() or fmt.Errorf()
 
-
-
 	//[lecture] -> Since error is an interface, you can define your own errors that include additinal information for logging or error handling.
 
+	//[Lecture] -> If ou need to merge multiple errors into a single error. That's what the errors.Join function is for:
 
+}
 
-  //[Lecture] -> If ou need to merge multiple errors into a single error. That's what the errors.Join function is for:
+func main() {
 
+	//? Sentinel error solution
+	ids := []int{1, 0, -3}
+
+	for _, id := range ids {
+		err := panicrecover.Solution2(id)
+		if errors.Is(err, panicrecover.ErrInvalidID) {
+			fmt.Println("Invalid ID found")
+		} else {
+			fmt.Printf("Id %d is valid.\n", id)
+		}
+	}
+
+	//? Custom error solution
+	name := ""
+	err := panicrecover.Solution3(name)
+	if err != nil {
+		var emptyFieldErr *panicrecover.EmptyFieldError
+		if errors.As(err, &emptyFieldErr) {
+			fmt.Printf("Error: %v\n", emptyFieldErr)
+			fmt.Printf("Field causing error: %s\n", emptyFieldErr.FieldName)
+		} else {
+			fmt.Println("No empty field error found")
+		}
+	} else {
+		fmt.Println("No error found")
+	}
 }
 
 
